fde6e9d32666512ecb7d1cc2031cd7d72d82d593
FBudn <111660524+FBudn@users.noreply.github.com>
2023-01-23 21:56:19 +0100

    correctly-working-eslint-and-github-actions :)

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -13,5 +13,5 @@ const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
 
 export default Button;
 
-// onClick={() => handleValues}1 kolejny test
+// onClick={() => handleValues}1 kolejny test2
 // tu w funkcji ktora przekazuje do Buttona mogę sobie spiąć w obiekt stany email itd zeby wyswietlic w 1
