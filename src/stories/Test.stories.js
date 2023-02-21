450ad1fb02f148636c5e1758c5bc0729b8a6b669
FBudn <111660524+FBudn@users.noreply.github.com>
2023-02-21 12:07:32 +0100

    working localhost storybook and localhost npm

---
+++ src/stories/Test.stories.js
@@ -0,0 +1,9 @@
+import Button from "../Atoms/Button/Button";
+
+export default {
+  title: "Test",
+  component: Button,
+};
+
+// eslint-disable-next-line react/react-in-jsx-scope, react/jsx-filename-extension, @typescript-eslint/explicit-function-return-type
+export const Red = () => <Button> 2 </Button>;
