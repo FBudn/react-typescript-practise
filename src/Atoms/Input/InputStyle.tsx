4a9788f9fe1e44113ba81a18d6b1135bcb26707c
FBudn <111660524+FBudn@users.noreply.github.com>
2023-03-01 18:51:54 +0100

    input width fixed in storybook

--- src/Atoms/Input/InputStyle.tsx
+++ src/Atoms/Input/InputStyle.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const InputAtom = styled.input`
   display: flex;
+  width: 100%;
   border-radius: 5px;
   height: 40px;
   border: 2px solid rgb(214 211 209);
