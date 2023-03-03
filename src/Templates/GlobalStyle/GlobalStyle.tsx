0b42ea8e3b6a3c82009877a4307b025217a0eb67
FBudn <111660524+FBudn@users.noreply.github.com>
2023-03-03 20:01:17 +0100

    StoryBook bug with padding fixed

--- src/Templates/GlobalStyle/GlobalStyle.tsx
+++ src/Templates/GlobalStyle/GlobalStyle.tsx
@@ -4,7 +4,9 @@ const GlobalStyle = createGlobalStyle`
 body {
     margin: 0;
     padding: 0;
+    layout: fullscreen;
 }
 `;
 
 export default GlobalStyle;
+export const parameters = { layout: "fullscreen" };
