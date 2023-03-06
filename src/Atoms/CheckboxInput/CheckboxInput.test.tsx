29d93409314ab141af9f958fcad968792a14f25f
FBudn <111660524+FBudn@users.noreply.github.com>
2023-03-06 10:22:05 +0100

    snapshot test to checkboxinput and toBeChecked()

--- src/Atoms/CheckboxInput/CheckboxInput.test.tsx
+++ src/Atoms/CheckboxInput/CheckboxInput.test.tsx
@@ -1,9 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { render, screen } from "@testing-library/react";
+import TestRenderer from "react-test-renderer";
 import CheckboxInput from "./CheckboxInput";
 
 test(`Should render CheckboxInput Component`, () => {
   render(<CheckboxInput />);
   const checkboxInputElement = screen.getByTestId(`CheckboxInput-1`);
   expect(checkboxInputElement).toBeInTheDocument();
+  expect(checkboxInputElement).not.toBeChecked();
+});
+
+test(`matches snapshot`, () => {
+  const tree = TestRenderer.create(<CheckboxInput />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
