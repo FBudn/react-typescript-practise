11efb8fb77070d7628f6ef6614815204e653823a
FBudn <111660524+FBudn@users.noreply.github.com>
2023-03-07 09:11:06 +0100

    input tests, without onchage-how many times called

--- src/Atoms/Input/Input.test.tsx
+++ src/Atoms/Input/Input.test.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 /* eslint-disable react/react-in-jsx-scope */
 import { render, screen } from "@testing-library/react";
 import TestRenderer from "react-test-renderer";
-// import user from "@testing-library/user-event";
+import user from "@testing-library/user-event";
 import Input from "./Input";
 import "jest-styled-components";
 
@@ -10,15 +11,27 @@ test(`Should render Input component`, () => {
   const InputElement = screen.getByTestId(`InputField-1`);
   expect(InputElement).toBeInTheDocument();
 });
-/* test(`Should store input value`, () => {
-  const mockInputRef = jest.fn();
-  const InputElement = screen.getByTestId(`InputField-1`);
-   user.type(InputElement);
-   render(<Input type="text" value={mockInputRef} />);
-   expect(mockInputRef).toBeCalledTimes(4);
-}); */
+test(`Should store input value`, () => {
+  // const mockInputRef = jest.fn();
+  render(<Input type="text" />);
+  const InputElement = screen.getByTestId(`InputField-1`) as HTMLInputElement;
+  InputElement.value = "Test";
+  expect(InputElement).toHaveValue("Test");
+  user.type(InputElement, "Test");
+  // InputElement.onchange = mockInputRef;
+  // expect(mockInputRef).toBeCalledTimes(4);
+
+  /* InputElement.value = "test";
+  expect(screen.getByDisplayValue("test")).toHaveAttribute(
+    "data-testid",
+    "InputField-1",
+  ); */
+});
 test(`Should match snapshot and have styles:`, () => {
   const InputElement = TestRenderer.create(<Input type="text" />).toJSON();
   expect(InputElement).toMatchSnapshot();
   expect(InputElement).toHaveStyleRule("display", "flex");
+  expect(InputElement).toHaveStyleRule("width", "100%");
+  expect(InputElement).toHaveStyleRule("border-radius", "5px");
+  expect(InputElement).toHaveStyleRule("height", "40px");
 });
