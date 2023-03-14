4fe9c5e336273634c45903cbdcdb5734c60eb715
FBudn <111660524+FBudn@users.noreply.github.com>
2023-03-14 13:59:29 +0100

    inputs width

--- src/Atoms/Input/Input.style.tsx
+++ src/Atoms/Input/Input.style.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const InputContainer = styled.input`
   display: flex;
-  width: 100%;
+  width: 99%;
   border-radius: 5px;
   height: 40px;
   border: 2px solid rgb(214 211 209);
